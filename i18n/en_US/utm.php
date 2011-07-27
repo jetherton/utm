e94c01a0673a63af7d22b49c955df01886cd987f
carjimdra <carjimdra@gmail.com>
2011-07-27 15:30:05 +0000

    add language file

---
+++ i18n/en_US/utm.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ *  Universal Transvers Mercator English Language file
+ *
+ * This plugin was written for Ushahidi Liberia, by the contractors of Ushahidi Liberia
+ * 2011
+ *
+ * @package  UTM plugin
+ * @author     Carter Draper <carjimdra@gmail.com>
+ * 
+ */
+
+$lang = array(
+'utm' => 'UTM Coordinates',
+'utm_type' => 'UTM',
+'utm_description' => 'f your location coordinates are in UTM, and not Lat, Lon, use this to convert them to Lat Lon and back and forth.',
+'hemispher' => 'Hemispher:',
+'zone' => 'Zone:',
+'easting' => 'Easting:',
+'northing' => 'Northing:',
+
+);
