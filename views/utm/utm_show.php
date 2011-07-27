487a38ca081e97f646903a98a7e36c0f4fa75d25
John Etherton <john.etherton@gmail.com>
2011-07-27 10:15:01 -0600

    Added some spaces between words

--- views/utm/utm_show.php
+++ views/utm/utm_show.php
@@ -1,4 +1,4 @@
 <li>
 	<small><?php echo Kohana::lang('utm.utm');?></small>
-	<?php echo  Kohana::lang('utm.utm_zone')." ". $zone.  Kohana::lang('utm.easting')." ". $easting.  Kohana::lang('utm.northing')." ". $northing; ?>
+	<?php echo  Kohana::lang('utm.utm_zone')." ". $zone. " ".  Kohana::lang('utm.easting')." ". $easting. " ". Kohana::lang('utm.northing')." ". $northing; ?>
 </li>
