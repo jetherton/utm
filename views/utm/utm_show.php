e8ea385d1e477bfae8a8053679dabe240fe436f5
John Etherton <john.etherton@gmail.com>
2011-08-31 15:05:16 -0600

    made the view work for Ushahidi 2.1

--- views/utm/utm_show.php
+++ views/utm/utm_show.php
@@ -1,4 +1,4 @@
-<li>
-	<small><?php echo Kohana::lang('utm.utm');?></small>
+<br/>
+<br/>
+	<h5 style="font-size:14px;border-top:1px dotted #c0c2b8;"><?php echo Kohana::lang('utm.utm');?></h5>
 	<?php echo  Kohana::lang('utm.utm_zone')." ". $zone. " ".  Kohana::lang('utm.easting')." ". $easting. " ". Kohana::lang('utm.northing')." ". $northing; ?>
-</li>
